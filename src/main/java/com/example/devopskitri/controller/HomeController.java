db19240a45c4d104d902d708351c6d36bed7e64c
jeonhaeyeong <71204532+hea0i@users.noreply.github.com>
2023-06-01 11:51:19 +0900

    Add files via upload

---
+++ src/main/java/com/example/devopskitri/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.devopskitri.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "안녕 데브옵스";
+    }
+}
